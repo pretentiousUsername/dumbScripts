48ffa9488deb4a8da0f5faa852efd77e152aa9f1
Ian Mitchell <mitchell.ian.2001@gmail.com>
2021-12-11 16:30:02 -0500

    Update cdk.py

--- CDK/cdk.py
+++ CDK/cdk.py
@@ -28,12 +28,11 @@ B = np.log(A)
 λ = u * B
 
 c1 = c0 * np.exp(-λ * t)
-D = "{:.8e}".format(c / c1)
+D = "{:.8e}".format(c1 / c)
 C1 = "{:.4e}".format(np.around(c1, decimals=4))
 C = "{:.4e}".format(np.around(c, decimals=4))
 
 print("The initial speed of light c0 = ", np.around(c0, decimals=4), "\n")
 print("The *real* speed of light is c = ", C, "\n")
-print("The speed of light is", C1, "m/s. c/c0 =", D)
+print("The speed of light is", C1, "m/s. c'/c =", D)
 # Since Λ has four significant digits, the calculation needs to be rounded.
-
